1ecf78bf2ad4b69b2a65704d1944b9b4a7f4d420
garciat5597 <60933658+garciat5597@users.noreply.github.com>
2020-02-13 11:07:49 -0600

    Rat animation
    he walk

---
+++ RoyalRodents/Assets/Scripts/SubjectScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Behavior script for rats in the player base.
+ * - CURRENT FUNCTIONALITY -
+ *      - Rats will run to whatever object is assigned and wait there.
+ *      
+ * - PLANNED FUTURE FUNCTIONALITY -
+ *      - Rats will be assigned buildings to run towards.
+ *      - After reaching their target, they will occupy that building.
+ *      - If not assigned a building, rats will run around the base.
+ */
+public class SubjectScript : MonoBehaviour
+{
+
+    public Animator ratAnim;
+    public float health = 30f;
+    public float moveSpeed = 0.5f;
+    public GameObject target;
+    private bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ratAnim = this.GetComponent<Animator>();
+        facingRight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if a target exists
+        if (target)
+        {
+            // Finds target object position, and then commands the rat to move if it is not very close to it already
+            Vector3 targetLocation = new Vector3(target.transform.position.x, 0, 0);
+            if(Mathf.Abs(targetLocation.x - transform.position.x) > 0.5f)
+            {
+                Move(targetLocation);
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+    // Moves the rat towards its target
+    void Move(Vector3 pos)
+    {
+        if(transform.position.x > pos.x)
+        {
+            if (facingRight)
+            {
+                flipDirection();
+            }
+
+            if(pos.x >= 0)
+            {
+                transform.position -= pos.normalized * Time.deltaTime * moveSpeed;
+            }
+            else
+            {
+                transform.position += pos.normalized * Time.deltaTime * moveSpeed;
+            }
+        }
+        else
+        {
+            if (!facingRight)
+            {
+                flipDirection();
+            }
+            if (pos.x >= 0)
+            {
+                transform.position += pos.normalized * Time.deltaTime * moveSpeed;
+            }
+            else
+            {
+                transform.position -= pos.normalized * Time.deltaTime * moveSpeed;
+            }
+        }
+    }
+
+    void flipDirection()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
